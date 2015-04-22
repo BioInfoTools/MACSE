93d4487aca725335453a2f850cfd00d8b6be7a52
Pavel <PavelBatusov@yandex.ru>
2015-04-22 14:37:31 +0300

    in-between commit

--- Source/clustering/cluster.py
+++ Source/clustering/cluster.py
@@ -17,6 +17,59 @@ def readFASTA(filename):
 				strings[len(strings)-1] += line[:len(line)-1]
 	return headers, strings
 
+def UPGMA(A, N, S, i = 0, j = 0):
+	def Align(s1, s2, subst="BLOSUM62"):
+		#reading score matrix
+		score_matrix = np.zeros((256, 256))
+		with open(substr) as file:
+			for line in file:
+				if line.startswith('#'): continue
+				#reading alphabet
+				alphabet = line.replace(' ', '')
+				break
+			#reading scores
+			for line in file:
+				
+		#initialize
+		F = np.zeros((len(s1[0])+1, len(s2[0])+1))
+		W = np.zeros((len(s1[0])+1, len(s2[0])+1))
+		#forward pass
+		for i in range(1, len(s1[0])+1):
+			for j in range(1, len(s2[0])+1):
+				way1 = F[i-1, j-1] + score_matrix[seq1[i-1]*128 + seq2[j-1]];
+			//порвать seq2
+			int way2 = F[(i-1)*m + j] + gap_extension;
+			if (W[(i-1)*m + j] == 1) way2 += gap_open;
+			//порвать seq1
+			int way3 = F[i*m + j-1] + gap_extension;
+			if (W[i*m + j-1] == 1) way3 += gap_open;
+			//выбираем максимум
+			if (way1 >= way2 && way1 >= way3) {
+				F[i*m + j] = way1;
+				W[i*m + j] = 1;
+			} else if (way2 >= way1 && way2 >= way3) {
+				F[i*m + j] = way2;
+				W[i*m + j] = 2;
+			} else {
+				F[i*m + j] = way3;
+				W[i*m + j] = 3;
+			}
+	if N == 1:
+		#finish
+		return S[0]
+	if i == j:
+		#search center clustering
+		max_value = A[0, 1]
+		for index1 in range(0, N):
+			for index2 in range(i + 1, N):
+				if max_value < A[index1, index2]:
+					i = index1
+					j = index2
+					max_value = A[i, j]
+	#creating a new cluster (i, j)
+	cls = Align(s[i], s[j])
+
+#MAIN
 if len(sys.argv) == 1: print "No files to align"
 for filename in sys.argv[1:]:
 	headers, sequences = readFASTA(filename)
@@ -40,7 +93,10 @@ for filename in sys.argv[1:]:
 				sys.exit()
 			A[i, j] = int(result[2])
 			S.append((result[0], result[1]))
-			print ''.join(map(lambda x: x + ' ', cmd))
-	print A
-	for elem in S:
-		print elem
+			#Debug print
+			#print ''.join(map(lambda x: x + ' ', cmd))
+	
+	#Debug out
+	#print A
+	#for elem in S:
+	#	print elem
