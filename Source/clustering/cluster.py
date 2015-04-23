68f10dc6924b2ecc9232a4ac4dc2c237abf836f0
Pavel <PavelBatusov@yandex.ru>
2015-04-23 18:51:36 +0300

    clustering with clustal omega

--- Source/clustering/cluster.py
+++ Source/clustering/cluster.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*- 
 
 import numpy as np
 import subprocess
@@ -17,86 +18,95 @@ def readFASTA(filename):
 				strings[len(strings)-1] += line[:len(line)-1]
 	return headers, strings
 
-def UPGMA(A, N, S, i = 0, j = 0):
-	def Align(s1, s2, subst="BLOSUM62"):
-		#reading score matrix
-		score_matrix = np.zeros((256, 256))
-		with open(substr) as file:
-			for line in file:
-				if line.startswith('#'): continue
-				#reading alphabet
-				alphabet = line.replace(' ', '')
-				break
-			#reading scores
-			for line in file:
-				
-		#initialize
-		F = np.zeros((len(s1[0])+1, len(s2[0])+1))
-		W = np.zeros((len(s1[0])+1, len(s2[0])+1))
-		#forward pass
-		for i in range(1, len(s1[0])+1):
-			for j in range(1, len(s2[0])+1):
-				way1 = F[i-1, j-1] + score_matrix[seq1[i-1]*128 + seq2[j-1]];
-			//порвать seq2
-			int way2 = F[(i-1)*m + j] + gap_extension;
-			if (W[(i-1)*m + j] == 1) way2 += gap_open;
-			//порвать seq1
-			int way3 = F[i*m + j-1] + gap_extension;
-			if (W[i*m + j-1] == 1) way3 += gap_open;
-			//выбираем максимум
-			if (way1 >= way2 && way1 >= way3) {
-				F[i*m + j] = way1;
-				W[i*m + j] = 1;
-			} else if (way2 >= way1 && way2 >= way3) {
-				F[i*m + j] = way2;
-				W[i*m + j] = 2;
-			} else {
-				F[i*m + j] = way3;
-				W[i*m + j] = 3;
-			}
-	if N == 1:
-		#finish
-		return S[0]
-	if i == j:
-		#search center clustering
-		max_value = A[0, 1]
-		for index1 in range(0, N):
-			for index2 in range(i + 1, N):
-				if max_value < A[index1, index2]:
-					i = index1
-					j = index2
-					max_value = A[i, j]
-	#creating a new cluster (i, j)
-	cls = Align(s[i], s[j])
+#alignment two blocks of sequences
+#using clustal omega to do align
+def align(data1, data2):
+	#create subprocess
+	child = subprocess.Popen(["clustalo","--in=-","--output-order=input-order"], \
+		stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+	#load sequences from first data block
+	for i in range(len(data1)):
+		if i % 2 == 0:
+			child.stdin.write('>'+str(data1[i])+'\n')
+		else:
+			child.stdin.write(data1[i]+'\n')
+	#load sequences from second data block
+	for i in range(len(data2)):
+		if i % 2 == 0:
+			child.stdin.write('>'+str(data2[i])+'\n')
+		else:
+			child.stdin.write(data2[i]+'\n')
+	#close stdin
+	child.stdin.close()
+	#save results
+	index = -1
+	result = data1 + data2
+	for line in iter(child.stdout.readline,''):
+		if line.startswith('>'): 
+			index += 2
+			result[index] = ""
+		else: 
+			if index > 0:
+				result[index] += line.rstrip()
+	return result
+
+#m - distance between clusters
+#s - align betwenn clusters
+#n - count of clusters
+def UPGMA(m, s, n):
+	if n == 2:
+		return s[1][0] # <- answer
+	#search for closest clusters
+	max_value, index1, index2 = 0, 0, 0
+	for i in range(n):
+		for j in range(i):
+			if m[i][j] > max_value:
+				index1, index2, max_value = i, j, m[i][j]
+	tek_align = s[index1][index2]
+	#updating distance & aligns
+	for i in range(n):
+		for j in range(i):
+			if not (i == index1 or j == index1) and (i == index2 or j == index2):
+				m[i][j] = (m[i][j] + max_value) / 2.0
+				s[i][j] = align(s[i][j], tek_align)
+	#delete columns & raws
+	for i in range(index1+1, n):
+		del m[i][index1]
+		del s[i][index1]
+	del m[index1]
+	del s[index1]
+	#next iteration
+	return UPGMA(m, s, n-1)
 
 #MAIN
 if len(sys.argv) == 1: print "No files to align"
 for filename in sys.argv[1:]:
+	#parsing input file
 	headers, sequences = readFASTA(filename)
+	#creating distance matrix
 	#each sequence is a single cluster
-	#calc first table
-	A = np.zeros((len(sequences), len(sequences)))
-	S = []
-	for i in range(len(sequences)):
-		for j in range(i + 1, len(sequences)):
+	matrix = []
+	aligns = []
+	for i in range(0, len(sequences)):
+		matrix.append([])
+		aligns.append([])
+		for j in range(0, i):
 			cmd = ['./pairwise', sequences[i], sequences[j]]
 			process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 			result = []
 			for line in process.stdout:
-				#last symbol is \n
-				result.append(line[:len(line)-1])
+				result.append(line.rstrip())
+			#check for an error
 			if len(result) != 3:
 				print "Error while trying to align sequences:"
-				print sequences[i]
-				print sequences[j]
+				print headers[i]
+				print headers[j]
 				print ''.join(map(lambda x: x + ' ', cmd))
 				sys.exit()
-			A[i, j] = int(result[2])
-			S.append((result[0], result[1]))
-			#Debug print
-			#print ''.join(map(lambda x: x + ' ', cmd))
+			#save data
+			aligns[i].append([headers[i], result[0], headers[j], result[1]])
+			matrix[i].append(int(result[2]))
+		matrix[i].append(0)
+	#clustering
+	UPGMA(matrix, aligns, len(sequences))
 	
-	#Debug out
-	#print A
-	#for elem in S:
-	#	print elem
